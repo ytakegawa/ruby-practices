50905fae1a5a8c375377c8250f678b1721e0b75c
ytakegawa <ytkgw1986@gmail.com>
2021-05-14 23:50:50 +0900

    Make fizzbuzz-program

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,13 @@
+numbers = (1..20).to_a
+
+numbers.each do |n|
+  if n % 3 == 0 && n % 5 == 0
+    puts "FizzBuzz"
+  elsif n % 3 == 0
+    puts "Fizz"
+  elsif n % 5 == 0
+    puts "Buzz"
+  else
+    puts n
+  end
+end
