b61ffacf427ce6b7de1b0329c81c1c7ffd87cd01
ytakegawa <ytkgw1986@gmail.com>
2021-05-27 14:02:32 +0900

    Correction calendar.rb

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -27,79 +27,25 @@ else
   end
 end
 
-today = Date.today
 start_day = Date.new(year, month, +1)
 last_day = Date.new(year, month, -1)
 
+# 年月週の装飾
 puts "       #{year}.#{month}"
 puts "Su Mo Tu We Th Fr Sa"
 
-#カレンダーの日付を整列させるプログラム
+# 最初の週の空欄表示
+start_day_space = "   " * start_day.wday
+print start_day_space
+
+# カレンダー表示
 (start_day..last_day).each do |d|
-  painted_today = Paint[d.day, :inverse]
-  if d == today && d.day == 1 && d.saturday?
-    puts "                   #{painted_today}"
-  elsif d == today && d.day == 1
-    position_of_start_day = d.cwday
-    case position_of_start_day
-    when 1
-      print "    #{painted_today} "
-    when 2
-      print "       #{painted_today} "
-    when 3
-      print "          #{painted_today} "
-    when 4
-      print "             #{painted_today} "
-    when 5
-      print "                #{painted_today} "
-      #when 6
-      #print "                   #{d.day}"
-    when 7
-      print " #{painted_today} "
-    end
-  elsif d == today && d.saturday?
-    if d.day <= 9
-      puts " #{painted_today} "
-    else
-      puts painted_today
-    end
-  elsif d == today
-    if d.day <= 9
-      print " #{painted_today} "
-    else
-      print "#{painted_today} "
-    end
-  elsif d.day == 1 && d.saturday?
-    puts "                   #{d.day}"
-  elsif d.day == 1
-    position_of_start_day = d.cwday
-    case position_of_start_day
-    when 1
-      print "    #{d.day} "
-    when 2
-      print "       #{d.day} "
-    when 3
-      print "          #{d.day} "
-    when 4
-      print "             #{d.day} "
-    when 5
-      print "                #{d.day} "
-      #when 6
-      #print "                   #{d.day}"
-    when 7
-      print " #{d.day} "
-    end
-  elsif d.saturday?
-    if d.day <= 9
-      puts " #{d.day} "
-    else
-      puts d.day
-    end
+  if d.day <= 9
+    print " "
+  end
+  if d.saturday?
+    print "#{d.day}\n"
   else
-    if d.day <= 9
-      print " #{d.day} "
-    else
-      print "#{d.day} "
-    end
+    print "#{d.day} "
   end
 end
