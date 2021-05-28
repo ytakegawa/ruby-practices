164bfcd8d823ea247cdaa99126940ab516c67747
ytakegawa <ytkgw1986@gmail.com>
2021-05-28 14:29:53 +0900

    Correction calendar.rb

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -2,8 +2,6 @@ require "date"
 require "optparse"
 require "paint"
 
-#-y,-mオプションがない場合は、現在の年月を代入し、オプションがある場合は、入力値を年月に代入するプログラム
-#オプションにありえない数字が入力された場合は、警告を表示しプログラム終了とする。
 options = ARGV.getopts("m:", "y:")
 if options["y"] == nil
   year = Date.today.year
@@ -29,6 +27,7 @@ end
 
 start_day = Date.new(year, month, +1)
 last_day = Date.new(year, month, -1)
+today = Date.today
 
 # 年月週の装飾
 puts "       #{year}.#{month}"
@@ -40,12 +39,21 @@ print start_day_space
 
 # カレンダー表示
 (start_day..last_day).each do |d|
-  if d.day <= 9
-    print " "
-  end
-  if d.saturday?
-    print "#{d.day}\n"
+  # 今日の日付の背景色を反転させるため、今日の日付を判定する条件分岐
+  if d == today
+    painted_today = Paint[d.day, :inverse]
+    # 日付が土曜日の場合は改行させるための条件分岐
+    if d.saturday?
+      printf("%2s\n", painted_today)
+    else
+      printf("%2s ", painted_today)
+    end
+  elsif d.saturday?
+    printf("%2d\n", d.day)
   else
-    print "#{d.day} "
+    printf("%2d ", d.day)
   end
 end
+
+# プログラムを実行したときに最後に表示される"%"を無くすため最後に改行入れる
+puts
