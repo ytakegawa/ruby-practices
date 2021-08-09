8c6cf43b28322f10e6d89bdf27b0f07aa3346df9
ytakegawa <ytkgw1986@gmail.com>
2021-08-09 15:58:38 +0900

    Create builde_elements method
    行数、単語数、バイト数の要素を抽出するメソッドを作成

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,30 +4,32 @@ require 'optparse'
 OPTIONS = ARGV.getopts('l')
 
 def main
-  output
-  total_calc if ARGV.find { |a| File.file?(a) } && ARGV.size >= 2
-end
-
-def output
-  generate_files_info.map.with_index do |file_info, i|
-    print file_info.count("\n").to_s.rjust(8)
+  elements = generate_files_info.map { |file_info| builde_elements(file_info) }
+  elements.each do |element|
+    print element[:col].to_s.rjust(8) # 行数の出力
     unless OPTIONS['l']
-      print file_info.split(/\s+/).size.to_s.rjust(8) # 単語数の出力
-      print file_info.bytesize.to_s.rjust(8) # バイト数の出力
+      print element[:word].to_s.rjust(8) # 単語数の出力
+      print element[:byte].to_s.rjust(8) # バイト数の出力
     end
-    print " #{ARGV[i]}" "\n"
+    print " #{element[:path]}" "\n" # ファイルパスの出力
   end
+  total_calc(elements) if ARGV.find { |a| File.file?(a) } && ARGV.size >= 2 # total値の出力
 end
 
-def total_calc
-  total_col = 0
-  total_word = 0
-  total_byte = 0
-  generate_files_info.map do |file_info|
-    total_col += file_info.count("\n")
-    total_word += file_info.split(/\s+/).size
-    total_byte += file_info.bytesize
-  end
+def builde_elements(file_info)
+  read_file = file_info.read
+  {
+    col: read_file.count("\n"),
+    word: read_file.split(/\s+/).size,
+    byte: read_file.bytesize,
+    path: File.path(file_info)
+  }
+end
+
+def total_calc(elements)
+  total_col = elements.sum { |element| element[:col] }
+  total_word = elements.sum { |element| element[:word] }
+  total_byte = elements.sum { |element| element[:byte] }
   if OPTIONS['l']
     puts "#{total_col.to_s.rjust(8)} total"
   else
@@ -40,7 +42,7 @@ def generate_files_info
   files_info = []
   if ARGV.find { |a| File.file?(a) } # コマンド引数にファイルパスがあるかどうか
     ARGV.each do |file_path|
-      files_info << File.open(file_path).read if File.file?(file_path) # ファイルパスがある場合は、それぞれのパスを配列に格納
+      files_info << File.open(file_path) if File.file?(file_path) # ファイルパスがある場合は、それぞれのパスを配列に格納
     end
   else
     files_info << $stdin.read # ファイルパスがない場合は、標準出力を配列に格納
