1d1a862a989ab309f9df42459fcc2e88de81097b
ytakegawa <ytkgw1986@gmail.com>
2021-08-17 14:14:09 +0900

    Modify const OPTIONS
    定数OPTIONSをローカル変数に修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,40 +1,19 @@
 # frozen_string_literal: true
 
-require 'optparse'
-OPTIONS = ARGV.getopts('l')
+require "optparse"
 
 def main
-  elements = generate_files_info.map { |file_info| builde_elements(file_info) }
+  options = ARGV.getopts("l")
+  elements = generate_files_info.map { |file_info| build_elements(file_info) }
   elements.each do |element|
     print element[:col].to_s.rjust(8) # 行数の出力
-    unless OPTIONS['l']
+    unless options["l"]
       print element[:word].to_s.rjust(8) # 単語数の出力
       print element[:byte].to_s.rjust(8) # バイト数の出力
     end
     print " #{element[:path]}" "\n" # ファイルパスの出力
   end
-  total_calc(elements) if ARGV.find { |a| File.file?(a) } && ARGV.size >= 2 # total値の出力
-end
-
-def builde_elements(file_info)
-  read_file = file_info.read
-  {
-    col: read_file.count("\n"),
-    word: read_file.split(/\s+/).size,
-    byte: read_file.bytesize,
-    path: File.path(file_info)
-  }
-end
-
-def total_calc(elements)
-  total_col = elements.sum { |element| element[:col] }
-  total_word = elements.sum { |element| element[:word] }
-  total_byte = elements.sum { |element| element[:byte] }
-  if OPTIONS['l']
-    puts "#{total_col.to_s.rjust(8)} total"
-  else
-    puts "#{total_col.to_s.rjust(8)}#{total_word.to_s.rjust(8)}#{total_byte.to_s.rjust(8)} total"
-  end
+  total_calc(elements, options) if ARGV.find { |a| File.file?(a) } && ARGV.size >= 2 # total値の出力
 end
 
 # コマンド引数の処理
@@ -48,6 +27,27 @@ def generate_files_info
     files_info << $stdin.read # ファイルパスがない場合は、標準出力を配列に格納
   end
   files_info
+end
+
+def build_elements(file_info)
+  read_file = file_info.read
+  {
+    col: read_file.count("\n"),
+    word: read_file.split(/\s+/).size,
+    byte: read_file.bytesize,
+    path: File.path(file_info),
+  }
+end
+
+def total_calc(elements, options)
+  total_col = elements.sum { |element| element[:col] }
+  total_word = elements.sum { |element| element[:word] }
+  total_byte = elements.sum { |element| element[:byte] }
+  if options["l"]
+    puts "#{total_col.to_s.rjust(8)} total"
+  else
+    puts "#{total_col.to_s.rjust(8)}#{total_word.to_s.rjust(8)}#{total_byte.to_s.rjust(8)} total"
+  end
 end
 
 main
