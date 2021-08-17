0ded5974a5c952eeade8b31190320c7a64d3c8c2
ytakegawa <ytkgw1986@gmail.com>
2021-08-17 18:22:32 +0900

    Modify behavior $stdin
    標準入力の場合の挙動を修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -8,22 +8,16 @@ def main
     line = element[:line]
     word = element[:word]
     byte = element[:byte]
-    path = element[:path]
+    path = File.path(element[:path]) if File.file?(element[:path]) # $stdinの場合pathが不要なので、path:の値がファイルオブジェクトの場合にpathを表示させる
     display_elements(line, word, byte, path)
   end
-  total_calc(elements) if ARGV.find { |a| File.file?(a) } && ARGV.size >= 2 # total値の出力
+  total_calc(elements) if ARGV.size >= 2 # コマンド引数が2つ以上の場合、total値の出力
 end
 
 # コマンド引数の処理
 def generate_files_info
   files_info = []
-  if ARGV.find { |a| File.file?(a) } # コマンド引数にファイルパスがあるかどうか
-    ARGV.each do |file_path|
-      files_info << File.open(file_path) if File.file?(file_path) # ファイルパスがある場合は、それぞれのパスを配列に格納
-    end
-  else
-    files_info << $stdin.read # ファイルパスがない場合は、標準出力を配列に格納
-  end
+  ARGV.size >= 1 ? ARGV.each { |file_path| files_info << File.open(file_path) } : files_info << $stdin
   files_info
 end
 
@@ -33,8 +27,18 @@ def build_elements(file_info)
     line: read_file.count("\n"),
     word: read_file.split(/\s+/).size,
     byte: read_file.bytesize,
-    path: File.path(file_info),
+    path: file_info,
   }
+end
+
+def display_elements(line, word, byte, path)
+  options = ARGV.getopts("l")
+  print trim_space(line) # 行数の出力
+  unless options["l"]
+    print trim_space(word) # 単語数の出力
+    print trim_space(byte) # バイト数の出力
+  end
+  print " #{path}" "\n" # ファイルパスの出力
 end
 
 def total_calc(elements)
@@ -45,17 +49,7 @@ def total_calc(elements)
   display_elements(line, word, byte, path)
 end
 
-def display_elements(line, word, byte, path) #line, word, byte, path, elements, options
-  options = ARGV.getopts("l")
-  print format(line) # 行数の出力
-  unless options["l"]
-    print format(word) # 単語数の出力
-    print format(byte) # バイト数の出力
-  end
-  print " #{path}" "\n" # ファイルパスの出力
-end
-
-def format(value)
+def trim_space(value)
   value.to_s.rjust(8)
 end
 
