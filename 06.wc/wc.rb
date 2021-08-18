0210adc9731d8e82f191ef86b461c15ce9b799e8
ytakegawa <ytkgw1986@gmail.com>
2021-08-18 22:39:07 +0900

    Correct path variable
    変数pathの代入値を修正。ARGVの値を直接代入する。

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,11 +4,11 @@ require "optparse"
 
 def main
   elements = command_arg_or_stdin_to_array.map { |object| build_elements(object) }
-  elements.each do |element|
+  elements.each_with_index do |element, i|
     line = element[:line]
     word = element[:word]
     byte = element[:byte]
-    path = File.path(element[:path]) if File.file?(element[:path]) # $stdinの場合filepathが不要なので、path:の値がファイルオブジェクトの場合にpathを表示させる
+    path = ARGV[i]
     display_elements(line, word, byte, path)
   end
   total_calc(elements) if ARGV.size >= 2 # コマンド引数が2つ以上の場合、total値の出力
@@ -27,7 +27,6 @@ def build_elements(object)
     line: txt_object.count("\n"),
     word: txt_object.split(/\s+/).size,
     byte: txt_object.bytesize,
-    path: object,
   }
 end
 
