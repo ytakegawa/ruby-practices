c889280e6976d95aa75bb83d39eedaae6e5bf4ed
ytakegawa <ytkgw1986@gmail.com>
2021-08-19 16:54:36 +0900

    Refactoring by rubocop
    rubocopの警告を解消

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require "optparse"
+require 'optparse'
 
 def main
-  options = ARGV.getopts("l")
+  options = ARGV.getopts('l')
   values = read_texts.map { |text_file| build_values(text_file) }
   values.each do |value|
     line, word, byte, path = %i[line word byte path].map { |key| value[key] }
@@ -21,13 +21,13 @@ def build_values(text_file)
     line: text_file[0].count("\n"),
     word: text_file[0].split(/\s+/).size,
     byte: text_file[0].bytesize,
-    path: text_file[1],
+    path: text_file[1]
   }
 end
 
 def display_values(line, word, byte, path, options)
   print format_value(line)
-  unless options["l"]
+  unless options['l']
     print format_value(word)
     print format_value(byte)
   end
@@ -40,7 +40,7 @@ end
 
 def calc_total_num(values, options)
   total_line, total_word, total_byte = %i[line word byte].map { |key| values.sum { |value| value[key] } }
-  path = "total"
+  path = 'total'
   display_values(total_line, total_word, total_byte, path, options)
 end
 
