05c39facf7c444815b442e0d9588661439592ac3
ytakegawa <ytkgw1986@gmail.com>
2022-03-25 17:45:03 +0900

    Refactoring: Remove duplicate code
    コードの重複を削除

--- 07.bowling_object/lib/frame.rb
+++ 07.bowling_object/lib/frame.rb
@@ -1,6 +1,8 @@
 require_relative 'shot'
 
 class Frame
+  attr_reader :first_shot, :second_shot
+
   def initialize(first_mark, second_mark, third_mark = nil, frame_index)
     @first_shot = Shot.new(first_mark)
     @second_shot = Shot.new(second_mark)
@@ -10,38 +12,19 @@ class Frame
 
   def score
     @first_shot.score + @second_shot.score + @third_shot.score
-    # if final_frame?
-    #   @first_shot.score + @second_shot.score + @third_shot.score
-    # else
-    #   @first_shot.score + @second_shot.score
-    # end
   end
 
   def strike?
-    @first_shot.score == 10 && @second_shot.score == 0 && !final_frame?
-  end
-
-  def spere?
-    @first_shot.score != 10 && @first_shot.score + @second_shot.score == 10 && !final_frame?
-  end
-
-  def next_frame_bonus
-    if strike?
-      @first_shot.score
-    else
-      @first_shot.score + @second_shot.score
+    unless final_frame?
+      @first_shot.score == 10
     end
   end
 
-  def next_first_shot_bonus
-    @first_shot.score
+  def spare?
+    unless final_frame?
+      @first_shot.score + @second_shot.score == 10
+    end
   end
-
-  def next_second_shot_bonus
-    @second_shot.score
-  end
-
-  #private
 
   def final_frame?
     @frame_index == 9
--- 07.bowling_object/lib/game.rb
+++ 07.bowling_object/lib/game.rb
@@ -1,5 +1,4 @@
 require_relative 'frame'
-require 'byebug'
 
 class Game
   def initialize(marks)
@@ -7,27 +6,26 @@ class Game
   end
 
   def score
-    #byebug
     point = 0
     @frames.each_with_index do |frame, i|
       this_frame = Frame.new(frame[0], frame[1], frame[2], i)
-      bonus_point = 0
       if this_frame.strike?
         next_frame = Frame.new(@frames[i + 1][0], @frames[i + 1][1], @frames[i + 1][2], i + 1)
-        bonus_point += next_frame.next_frame_bonus
+        point += next_frame.first_shot.score
         if next_frame.strike?
           if next_frame.final_frame?
-            bonus_point += next_frame.next_second_shot_bonus
+            point += next_frame.second_shot.score
           else
             after_next_frame = Frame.new(@frames[i + 2][0], @frames[i + 2][1], @frames[i + 2][2], i + 2)
-            bonus_point += after_next_frame.next_first_shot_bonus
+            point += after_next_frame.first_shot.score
           end
         end
-      elsif this_frame.spere?
+        point += next_frame.second_shot.score
+      elsif this_frame.spare?
         next_frame = Frame.new(@frames[i + 1][0], @frames[i + 1][1], @frames[i + 1][2], i + 1)
-        bonus_point += next_frame.next_first_shot_bonus
+        point += next_frame.first_shot.score
       end
-      point += this_frame.score + bonus_point
+      point += this_frame.score
     end
     point
   end
@@ -37,7 +35,7 @@ class Game
   def marks_to_frames(marks)
     pins = marks.split(',')
     frames = []
-    9.times do |i|
+    9.times do
       frame = pins.shift(2)
       if frame.first == 'X'
         frames << ['X', '0']
